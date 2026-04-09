fa0fd376a0666314a61662a3bdcbd38ec3f902aa
SHAOYIK YEOH <sooh.ooo@gmail.com>
2026-04-10 02:55:58 +0900

    add containsduplicate

---
+++ containsduplicate/contaiduplicacte_solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+#solution_1
+def containsDuplicate(nums: List[int]) :
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+    return False
+
+#solution_2
+def containsDuplicate2(nums:List[int]):
+    seen =set()
+    for i in nums:
+        if i in seen:
+            return True
+        seen.add(i)
+    return False
---
+++ containsduplicate/result.py
@@ -0,0 +1,17 @@
+from contaiduplicacte_solution import containsDuplicate2
+
+nums = [
+    "shadow_fox",
+    "pixel_wolf",
+    "neon_drift",
+    "frostbyte",
+    "lunar_echo",
+    "static_ray",
+    "iron_pulse",
+    "void_spark",
+    "cyber_monk",
+    "blaze_run"
+]
+
+
+print(containsDuplicate2(nums))
